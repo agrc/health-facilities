5bc1e101dc6de3c32f99cc0bcf0316f091bf7ea8
steveoh <sgourley@utah.gov>
2021-06-09 12:44:08 -0600

    style: lint with fix

--- src/app/tests/jasmineTestBootstrap.js
+++ src/app/tests/jasmineTestBootstrap.js
@@ -1,7 +1,7 @@
 /* global JasmineFaviconReporter, jasmineRequire */
-var dojoConfig = {  // eslint-disable-line no-unused-vars
+var dojoConfig = { // eslint-disable-line no-unused-vars
     baseUrl: '/src/',
-    packages: ['dojo',  // dojo is required here since we are defining baseUrl before loading dojo
+    packages: ['dojo', // dojo is required here since we are defining baseUrl before loading dojo
         {
             name: 'agrc-jasmine-matchers',
             location: 'agrc-jasmine-matchers/src'
